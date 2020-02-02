dc7c6134ab0e40a396ac0e91f8f510b2ed61df17
Chen Xu <chenxu@Chens-iMac.local>
2020-02-02 17:47:50 +0800

    mod

--- time_series_forecasting.py
+++ time_series_forecasting.py
@@ -15,3 +15,164 @@ zip_path = tf.keras.utils.get_file(
     fname='jena_climate_2009_2016.csv.zip',
     extract=True)
 csv_path, _ = os.path.splitext(zip_path)
+
+df = pd.read_csv(csv_path)
+
+def univariate_data(dataset, start_index, end_index, history_size, target_size):
+    data = []
+    labels = []
+
+    start_index = start_index + history_size
+    if end_index is None:
+        end_index = len(dataset) - target_size
+
+
+    for i in range(start_index, end_index):
+        indices = range(i-history_size, i)
+        # Reshape data from (history_size,) to (history_size, 1)
+        data.append(np.reshape(dataset[indices], (history_size, 1)))
+        labels.append(dataset[i+target_size])
+    return np.array(data), np.array(labels)[..., np.newaxis]
+
+TRAIN_SPLIT = 300000
+tf.random.set_seed(13)
+
+uni_data = df['T (degC)']
+uni_data.index = df['Date Time']
+uni_data.head()
+
+uni_data = uni_data.values
+
+uni_train_mean = uni_data[:TRAIN_SPLIT].mean()
+uni_train_std = uni_data[:TRAIN_SPLIT].std()
+
+uni_data = (uni_data-uni_train_mean)/uni_train_std
+
+univariate_past_history = 20
+univariate_future_target = 0
+
+x_train_uni, y_train_uni = univariate_data(uni_data, 0, TRAIN_SPLIT,
+                                           univariate_past_history,
+                                           univariate_future_target)
+x_val_uni, y_val_uni = univariate_data(uni_data, TRAIN_SPLIT, None,
+                                       univariate_past_history,
+                                       univariate_future_target)
+
+
+print ('Single window of past history')
+print (x_train_uni[0])
+print ('\n Target temperature to predict')
+print (y_train_uni[0])
+
+
+BATCH_SIZE = 256
+BUFFER_SIZE = 10000
+
+train_univariate = tf.data.Dataset.from_tensor_slices((x_train_uni, y_train_uni))
+train_univariate = train_univariate.cache().shuffle(BUFFER_SIZE).batch(BATCH_SIZE).repeat()
+
+val_univariate = tf.data.Dataset.from_tensor_slices((x_val_uni, y_val_uni))
+val_univariate = val_univariate.batch(BATCH_SIZE).repeat()
+
+simple_lstm_model = tf.keras.models.Sequential([
+    tf.keras.layers.LSTM(8, input_shape=x_train_uni.shape[-2:]),
+    tf.keras.layers.Dense(1)
+])
+simple_lstm_model.compile(optimizer="adam", loss="mae")
+
+EVALUATION_INTERVAL = 200
+EPOCHS = 10
+
+"""
+simple_lstm_model.fit(train_univariate, epochs=EPOCHS,
+                      steps_per_epoch=EVALUATION_INTERVAL,
+                      validation_data=val_univariate, validation_steps=50)
+"""
+
+
+features_considered = ['p (mbar)', 'T (degC)', 'rho (g/m**3)']
+features = df[features_considered]
+features.index = df['Date Time']
+features.head()
+
+dataset = features.values
+data_mean = dataset[:TRAIN_SPLIT].mean(axis=0)
+data_std = dataset[:TRAIN_SPLIT].std(axis=0)
+
+
+def multivariate_data(dataset, target, start_index, end_index, history_size,
+                     target_size, step, single_step=False):
+    data, labels = [], []
+
+    start_index = start_index + history_size
+    if end_index is None:
+        end_index = len(dataset) - target_size
+
+    for i in range(start_index, end_index):
+        indices = range(i-history_size, i, step)
+        data.append(dataset[indices])
+
+        if single_step:
+            labels.append(target[i+target_size])
+        else:
+            labels.append(target[i:i+target_size])
+
+    return np.array(data), np.array(labels)
+
+
+past_history = 720
+future_target = 72
+STEP = 6
+
+x_train_single, y_train_single = multivariate_data(dataset, dataset[:, 1], 0,
+                                                   TRAIN_SPLIT, past_history,
+                                                   future_target, STEP,
+                                                   single_step=True)
+x_val_single, y_val_single = multivariate_data(dataset, dataset[:, 1],
+                                                 TRAIN_SPLIT, None, past_history,
+                                                 future_target, STEP,
+                                                 single_step=True)
+
+
+train_data_single = tf.data.Dataset.from_tensor_slices((x_train_single, y_train_single))
+train_data_single = train_data_single.cache().shuffle(BUFFER_SIZE).batch(BATCH_SIZE).repeat()
+
+val_data_single = tf.data.Dataset.from_tensor_slices((x_val_single, y_val_single))
+val_data_single = val_data_single.batch(BATCH_SIZE).repeat()
+
+single_step_model = tf.keras.models.Sequential()
+single_step_model.add(tf.keras.layers.LSTM(32, input_shape=x_train_single.shape[-2:]))
+single_step_model.add(tf.keras.layers.Dense(1))
+single_step_model.compile(optimizer=tf.keras.optimizers.RMSprop(), loss="mae")
+
+"""
+single_step_history = single_step_model.fit(train_data_single, epochs=EPOCHS,
+                                            steps_per_epoch=EVALUATION_INTERVAL,
+                                            validation_data=val_data_single,
+                                            validation_steps=50)
+"""
+
+x_train_multi, y_train_multi = multivariate_data(dataset, dataset[:, 1], 0,
+                                                   TRAIN_SPLIT, past_history,
+                                                   future_target, STEP)
+x_val_multi, y_val_multi = multivariate_data(dataset, dataset[:, 1],
+                                                 TRAIN_SPLIT, None, past_history,
+                                                 future_target, STEP)
+
+train_data_multi = tf.data.Dataset.from_tensor_slices((x_train_multi, y_train_multi))
+train_data_multi = train_data_multi.cache().shuffle(BUFFER_SIZE).batch(BATCH_SIZE).repeat()
+
+val_data_multi = tf.data.Dataset.from_tensor_slices((x_val_multi, y_val_multi))
+val_data_multi = val_data_multi.batch(BATCH_SIZE).repeat()
+
+multi_step_model = tf.keras.models.Sequential()
+multi_step_model.add(tf.keras.layers.LSTM(32, return_sequences=True,
+                                          input_shape=x_train_multi.shape[-2:]))
+multi_step_model.add(tf.keras.layers.LSTM(16, activation="relu"))
+multi_step_model.add(tf.keras.layers.Dense(72))
+multi_step_model.compile(optimizer=tf.keras.optimizers.RMSprop(clipvalue=1.0), loss="mae")
+
+multi_step_history = multi_step_model.fit(train_data_multi, epochs=EPOCHS,
+                                          steps_per_epoch=EVALUATION_INTERVAL,
+                                          validation_data=val_data_multi,
+                                          validation_steps=50)
