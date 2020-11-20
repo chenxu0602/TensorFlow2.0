3cfe64a4229733e109bc2890c723dc854369f67d
Chen Xu <chenxu@Chens-MacBook-Air.local>
2020-11-20 15:41:11 +0800

    mod

---
+++ neural_machine_translation_with_attention.py
@@ -0,0 +1,86 @@
+import tensorflow as tf
+
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+from sklearn.model_selection import train_test_split
+
+import unicodedata
+import re
+import numpy as np
+import os
+import io
+import time
+
+path_to_zip = tf.keras.utils.get_file(
+    "spa-eng.zip", origin="http://storage.googleapis.com/download.tensorflow.org/data/spa-eng.zip",
+    extract=True)
+
+path_to_file = os.path.dirname(path_to_zip) + "/spa-eng/spa.txt"
+
+def unicode_to_ascii(s):
+    return ''.join(c for c in unicodedata.normalize("NFD", s) if unicodedata.category(c) != "Mn")
+
+def preprocess_sentence(w):
+    w = unicode_to_ascii(w.lower().strip())
+
+    w = re.sub(r"([?.!,¿])", r" \1 ", w)
+    w = re.sub(r'[" "]+', " ", w)    
+
+    w = re.sub(r"[^a-zA-Z?.!,¿]+", " ", w)
+
+    w = w.strip()
+
+    w = '<start> ' + w + ' <end>'
+    return w
+
+
+en_sentence = u"May I borrow this book?"
+sp_sentence = u"¿Puedo tomar prestado este libro?"
+print(preprocess_sentence(en_sentence))
+print(preprocess_sentence(sp_sentence).encode('utf-8'))
+
+
+def create_datasets(path, num_examples):
+    lines = io.open(path, encoding='UTF-8').read().strip().split('\n')
+    word_pairs = [[preprocess_sentence(w) for w in l.split('\t')] for l in lines[:num_examples]]
+    return zip(*word_pairs)
+
+
+en, sp = create_datasets(path_to_file, None)
+print(en[-1])
+print(sp[-1])
+
+def tokenize(lang):
+    lang_tokenizer = tf.keras.preprocessing.text.Tokenizer(filters='')
+    lang_tokenizer.fit_on_texts(lang)
+    tensor = lang_tokenizer.texts_to_sequences(lang)
+    tensor = tf.keras.preprocessing.sequence.pad_sequences(tensor, padding="post")
+    return tensor, lang_tokenizer
+
+def load_dataset(path, num_examples=None):
+    targ_lang, inp_lang = create_datasets(path, num_examples)
+
+    input_tensor, inp_lang_tokenizer = tokenize(inp_lang)
+    target_tensor, targ_lang_tokenizer = tokenize(targ_lang)
+
+    return input_tensor, target_tensor, inp_lang_tokenizer, targ_lang_tokenizer
+
+num_examples = 30000
+input_tensor, target_tensor, inp_lang, targ_lang = load_dataset(path_to_file, num_examples)
+
+max_length_targ, max_length_inp = target_tensor.shape[1], input_tensor.shape[1]
+
+input_tensor_train, input_tensor_val, target_tensor_train, target_tensor_val = train_test_split(input_tensor, target_tensor, test_size=0.2)
+print(len(input_tensor_train), len(target_tensor_train), len(input_tensor_val), len(target_tensor_val))
+
+
+def convert(lang, tensor):
+    for t in tensor:
+        if t != 0:
+            print ("%d ----> %s" % (t, lang.index_word[t]))
+
+print ("Input Language; index to word mapping")
+convert(inp_lang, input_tensor_train[0])
+print ()
+print ("Target Language; index to word mapping")
+convert(targ_lang, target_tensor_train[0])
