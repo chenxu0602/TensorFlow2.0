a31f9a904ec0a5f47c6b1642548d797cb43b86f8
Chen Xu <chenxu@Chens-iMac.local>
2020-12-21 20:06:13 +0800

    mod

--- tf_ts.py
+++ tf_ts.py
@@ -710,3 +710,97 @@ multi_val_performance['LSTM'] = multi_lstm_model.evaluate(multi_window.val)
 multi_performance['LSTM'] = multi_lstm_model.evaluate(multi_window.test, verbose=0)
 multi_window.plot(multi_lstm_model)
                           
+class FeedBack(tf.keras.Model):
+    def __init__(self, units, out_steps):
+        super().__init__()
+        self.out_steps = out_steps
+        self.units = units
+        self.lstm_cell = tf.keras.layers.LSTMCell(units)
+        # Also wrap the LSTMCell in an RNN to simplify the `warmup` method.
+        self.lstm_rnn = tf.keras.layers.RNN(self.lstm_cell, return_state=True)
+        self.dense = tf.keras.layers.Dense(num_features)
+
+    def warmup(self, inputs):
+        # inputs.shape => (batch, time, features)
+        # x.shape => (batch, lstm_units)
+        x, *state = self.lstm_rnn(inputs)
+
+        # predictions.shape => (batch, features)
+        prediction = self.dense(x)
+        return prediction, state
+
+    def call(self, inputs, training=None):
+        # Use a TensorArray to capture dynamically unrolled outputs.
+        predictions = []
+        # Initialize the lstm state
+        prediction, state = self.warmup(inputs)
+
+        # Insert the first prediction
+        predictions.append(prediction)
+
+        # Run the rest of the prediction steps
+        for n in range(1, self.out_steps):
+            # Use the last prediction as input.
+            x = prediction
+            # Execute one lstm step.
+            x, state = self.lstm_cell(x, states=state,
+                                      training=training)
+            # Convert the lstm output to a prediction.
+            predicton = self.dense(x)
+            # Add the prediction to the output
+            predictions.append(prediction)
+
+        # predictions.shape => (time, batch, features)
+        predictions = tf.stack(predictions)
+        # predictions.shape => (batch, time, features)
+        predictions = tf.transpose(predictions, [1, 0, 2])
+        return predictions
+
+feedback_model = FeedBack(units=32, out_steps=OUT_STEPS)
+prediction, state = feedback_model.warmup(multi_window.example[0])
+
+print('Output shape (batch, time, features): ', feedback_model(multi_window.example[0]).shape)
+
+MAX_EPOCHS = 20
+
+def compile_and_fit(model, window, patience=2):
+    early_stopping = tf.keras.callbacks.EarlyStopping(monitor="val_loss",
+                                                      patience=patience,
+                                                      mode="min")
+
+    model.compile(loss=tf.losses.MeanSquaredError(),
+                  optimizer=tf.optimizers.Adam(),
+                  metrics=[tf.metrics.MeanAbsoluteError()])
+
+    history = model.fit(window.train, epochs=MAX_EPOCHS,
+                        validation_data=window.val,
+                        callbacks=[early_stopping])
+
+    return history
+
+history = compile_and_fit(feedback_model, multi_window)
+
+multi_val_performance['AR LSTM'] = feedback_model.evaluate(multi_window.val)
+multi_performance['AR LSTM'] = feedback_model.evaluate(multi_window.test, verbose=0)
+multi_window.plot(feedback_model)
+
+
+x = np.arange(len(multi_performance))
+width = 0.3
+
+
+metric_name = 'mean_absolute_error'
+metric_index = lstm_model.metrics_names.index('mean_absolute_error')
+val_mae = [v[metric_index] for v in multi_val_performance.values()]
+test_mae = [v[metric_index] for v in multi_performance.values()]
+
+plt.bar(x - 0.17, val_mae, width, label='Validation')
+plt.bar(x + 0.17, test_mae, width, label='Test')
+plt.xticks(ticks=x, labels=multi_performance.keys(),
+           rotation=45)
+plt.ylabel(f'MAE (average over all times and outputs)')
+_ = plt.legend()
+
+
+for name, value in multi_performance.items():
+    print(f'{name:8s}: {value[1]:0.4f}')
---
+++ tf_ts_autoregressive.py
@@ -0,0 +1,264 @@
+import os
+import datetime
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import tensorflow as tf
+
+mpl.rcParams['figure.figsize'] = (8, 6)
+mpl.rcParams['axes.grid'] = False
+
+zip_path = tf.keras.utils.get_file(
+    origin='https://storage.googleapis.com/tensorflow/tf-keras-datasets/jena_climate_2009_2016.csv.zip',
+    fname='jena_climate_2009_2016.csv.zip',
+    extract=True)
+csv_path, _ = os.path.splitext(zip_path)
+
+df = pd.read_csv(csv_path)
+# slice [start:stop:step], starting from index 5 take every 6th record.
+df = df[5::6]
+
+date_time = pd.to_datetime(df.pop('Date Time'), format='%d.%m.%Y %H:%M:%S')
+
+wv = df.pop('wv (m/s)')
+max_wv = df.pop('max. wv (m/s)')
+
+# Convert to radians.
+wd_rad = df.pop('wd (deg)')*np.pi / 180
+
+# Calculate the wind x and y components.
+df['Wx'] = wv*np.cos(wd_rad)
+df['Wy'] = wv*np.sin(wd_rad)
+
+# Calculate the max wind x and y components.
+df['max Wx'] = max_wv*np.cos(wd_rad)
+df['max Wy'] = max_wv*np.sin(wd_rad)
+
+
+timestamp_s = date_time.map(datetime.datetime.timestamp)
+
+day = 24*60*60
+year = (365.2425)*day
+
+df['Day sin'] = np.sin(timestamp_s * (2 * np.pi / day))
+df['Day cos'] = np.cos(timestamp_s * (2 * np.pi / day))
+df['Year sin'] = np.sin(timestamp_s * (2 * np.pi / year))
+df['Year cos'] = np.cos(timestamp_s * (2 * np.pi / year))
+
+
+column_indices = {name: i for i, name in enumerate(df.columns)}
+
+n = len(df)
+train_df = df[0:int(n*0.7)]
+val_df = df[int(n*0.7):int(n*0.9)]
+test_df = df[int(n*0.9):]
+
+num_features = df.shape[1]
+
+train_mean = train_df.mean()
+train_std = train_df.std()
+
+train_df = (train_df - train_mean) / train_std
+val_df = (val_df - train_mean) / train_std
+test_df = (test_df - train_mean) / train_std
+
+
+class WindowGenerator():
+    def __init__(self, input_width, label_width, shift,
+                 train_df=train_df, val_df=val_df, test_df=test_df,
+                 label_columns=None):
+        self.train_df = train_df
+        self.val_df = val_df
+        self.test_df = test_df
+
+        self.label_columns = label_columns
+        if label_columns is not None:
+            self.label_columns_indices = {name: i for i, name in enumerate(label_columns)}
+        self.column_indices = {name: i for i, name in enumerate(train_df.columns)}
+
+        self.input_width = input_width
+        self.label_width = label_width
+        self.shift = shift
+
+        self.total_window_size = input_width + shift
+
+        self.input_slice = slice(0, input_width)
+        self.input_indices = np.arange(self.total_window_size)[self.input_slice]
+
+        self.label_start = self.total_window_size - self.label_width
+        self.labels_slice = slice(self.label_start, None)
+        self.label_indices = np.arange(self.total_window_size)[self.labels_slice]
+
+    def __repr__(self):
+        return '\n'.join([
+            f'Total window size: {self.total_window_size}',
+            f'Input indices: {self.input_indices}',
+            f'Label indices: {self.label_indices}',
+            f'Label column name(s): {self.label_columns}'])
+
+    def split_window(self, features):
+        inputs = features[:, self.input_slice, :]
+        labels = features[:, self.labels_slice, :]
+        if self.label_columns is not None:
+            labels = tf.stack(
+                [labels[:, :, self.column_indices[name]] for name in self.label_columns],
+                axis=-1)
+
+        inputs.set_shape([None, self.input_width, None])
+        labels.set_shape([None, self.label_width, None])
+
+        return inputs, labels
+
+    def plot(self, model=None, plot_col="T (degC)", max_subplots=3):
+        inputs, labels = self.example
+        plt.figure(figsize=(12, 8))
+        plot_col_index = self.column_indices[plot_col]
+        max_n = min(max_subplots, len(inputs))
+        for n in range(max_n):
+            plt.subplot(max_n, 1, n + 1)
+            plt.ylabel(f"{plot_col} [normed]")
+            plt.plot(self.input_indices, inputs[n, :, plot_col_index],
+                     label="Inputs", marker='.', zorder=-10)
+
+            if self.label_columns:
+                label_col_index = self.label_columns.indices.get(plot_col, None)
+            else:
+                label_col_index = plot_col_index
+
+            if label_col_index is None:
+                continue
+
+            plt.scatter(self.label_indices, labels[n, :, label_col_index],
+                        edgecolors='k', label="Labels", c="#2ca02c", s=64)
+
+            
+            if model is not None:
+                predictions = model(inputs)
+                plt.scatter(self.label_indices, predictions[n, :, label_col_index],
+                            marker='X', edgecolor='k', label="Predictions",
+                            c="#ff7f0e", s=64)
+
+            if n == 0:
+                plt.legend()
+
+        plt.xlabel("Time [h]")
+
+
+    def make_dataset(self, data):
+        data = np.array(data, dtype=np.float32)
+        ds = tf.keras.preprocessing.timeseries_dataset_from_array(
+            data=data,
+            targets=None,
+            sequence_length=self.total_window_size,
+            sequence_stride=1,
+            shuffle=True,
+            batch_size=32,)
+        ds = ds.map(self.split_window)
+        return ds
+
+    @property
+    def train(self):
+        return self.make_dataset(self.train_df)
+
+    @property
+    def val(self):
+        return self.make_dataset(self.val_df)
+
+    @property
+    def test(self):
+        return self.make_dataset(self.test_df)
+
+    @property
+    def example(self):
+        result = getattr(self, "_example", None)
+        if result is None:
+            result = next(iter(self.train))
+            self._example = result
+        return result
+
+OUT_STEPS = 24
+multi_window = WindowGenerator(input_width=24,
+                              label_width=OUT_STEPS,
+                              shift=OUT_STEPS)
+
+class FeedBack(tf.keras.Model):
+    def __init__(self, units, out_steps):
+        super().__init__()
+        self.out_steps = out_steps
+        self.units = units
+        self.lstm_cell = tf.keras.layers.LSTMCell(units)
+        # Also wrap the LSTMCell in an RNN to simplify the `warmup` method.
+        self.lstm_rnn = tf.keras.layers.RNN(self.lstm_cell, return_state=True)
+        self.dense = tf.keras.layers.Dense(num_features)
+
+    def warmup(self, inputs):
+        # inputs.shape => (batch, time, features)
+        # x.shape => (batch, lstm_units)
+        x, *state = self.lstm_rnn(inputs)
+
+        # predictions.shape => (batch, features)
+        prediction = self.dense(x)
+        return prediction, state
+
+    def call(self, inputs, training=None):
+        # Use a TensorArray to capture dynamically unrolled outputs.
+        predictions = []
+        # Initialize the lstm state
+        prediction, state = self.warmup(inputs)
+
+        # Insert the first prediction
+        predictions.append(prediction)
+
+        # Run the rest of the prediction steps
+        for n in range(1, self.out_steps):
+            # Use the last prediction as input.
+            x = prediction
+            # Execute one lstm step.
+            x, state = self.lstm_cell(x, states=state,
+                                      training=training)
+            # Convert the lstm output to a prediction.
+            predicton = self.dense(x)
+            # Add the prediction to the output
+            predictions.append(prediction)
+
+        # predictions.shape => (time, batch, features)
+        predictions = tf.stack(predictions)
+        # predictions.shape => (batch, time, features)
+        predictions = tf.transpose(predictions, [1, 0, 2])
+        return predictions
+
+feedback_model = FeedBack(units=32, out_steps=OUT_STEPS)
+prediction, state = feedback_model.warmup(multi_window.example[0])
+
+print('Output shape (batch, time, features): ', feedback_model(multi_window.example[0]).shape)
+
+MAX_EPOCHS = 20
+
+def compile_and_fit(model, window, patience=2):
+    early_stopping = tf.keras.callbacks.EarlyStopping(monitor="val_loss",
+                                                      patience=patience,
+                                                      mode="min")
+
+    model.compile(loss=tf.losses.MeanSquaredError(),
+                  optimizer=tf.optimizers.Adam(),
+                  metrics=[tf.metrics.MeanAbsoluteError()])
+
+    history = model.fit(window.train, epochs=MAX_EPOCHS,
+                        validation_data=window.val,
+                        callbacks=[early_stopping])
+
+    return history
+
+history = compile_and_fit(feedback_model, multi_window)
+
+multi_val_performance, multi_performance = {}, {}
+
+multi_val_performance['AR LSTM'] = feedback_model.evaluate(multi_window.val)
+multi_performance['AR LSTM'] = feedback_model.evaluate(multi_window.test, verbose=0)
+multi_window.plot(feedback_model)
+
+
+
